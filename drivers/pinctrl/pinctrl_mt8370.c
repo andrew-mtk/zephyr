461949c2ac64dd7751180b5aaef7d009050ae20c
Felix Freimann <felix.freimann@mediatek.com>
2026-02-26 15:03:25 -0800

    Refactor PINCTRL
    Refactored PINCTRL for MT8365 (G350), MT8370 (G510) and MT8390 (G700) as well as removed obselete / commented-out macros.

--- drivers/pinctrl/pinctrl_mt8370.c
+++ drivers/pinctrl/pinctrl_mt8370.c
@@ -10,53 +10,9 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/pinctrl.h>
 
-#define PINCTRL_BASE_ADDR DT_INST_REG_ADDR(0)
+#include "pinctrl_mtk_common.h"
 
-// #define PINCTRL_OFFSET_EH_CF				0x0050
-// #define PINCTRL_OFFSET_RESL_CFG	            0x0060
-// #define PINCTRL_OFFSET_PUPU_CFG_0			0x0070
-// #define PINCTRL_OFFSET_PUPU_CFG_0_SET		0x0074
-// #define PINCTRL_OFFSET_PUPU_CFG_0_CLR		0x0078
-// #define PINCTRL_OFFSET_PUPU_CFG_1			0x0080
-// #define PINCTRL_OFFSET_PUPU_CFG_1_SET		0x0084
-// #define PINCTRL_OFFSET_PUPU_CFG_1_CLR		0x0088
-// #define PINCTRL_OFFSET_PUPU_CFG_2			0x0090
-// #define PINCTRL_OFFSET_PUPU_CFG_2_SET		0x0094
-// #define PINCTRL_OFFSET_PUPU_CFG_2_CLR		0x0098
-// #define PINCTRL_OFFSET_DOUT_0				0x00a0
-// #define PINCTRL_OFFSET_DOUT_0_SET			0x00a4
-// #define PINCTRL_OFFSET_DOUT_0_CLR			0x00a8
-// #define PINCTRL_OFFSET_DOUT_1				0x00b0
-// #define PINCTRL_OFFSET_DOUT_1_SET			0x00b4
-// #define PINCTRL_OFFSET_DOUT_1_CLR			0x00b8
-// #define PINCTRL_OFFSET_DOUT_2				0x00c0
-// #define PINCTRL_OFFSET_DOUT_2_SET			0x00c4
-// #define PINCTRL_OFFSET_DOUT_2_CLR			0x00c8
-// #define PINCTRL_OFFSET_DOUT_3				0x00d0
-// #define PINCTRL_OFFSET_DOUT_3_SET			0x00d4
-// #define PINCTRL_OFFSET_DOUT_3_CLR			0x00d8
-// #define PINCTRL_OFFSET_DOUT_4				0x00e0
-// #define PINCTRL_OFFSET_DOUT_4_SET			0x00e4
-// #define PINCTRL_OFFSET_DOUT_4_CLR			0x00e8
-// #define PINCTRL_OFFSET_PUPU_CFG_3			0x00f0
-// #define PINCTRL_OFFSET_PUPU_CFG_3_SET		0x00f4
-// #define PINCTRL_OFFSET_PUPU_CFG_3_CLR		0x00f8
-// #define PINCTRL_OFFSET_DEBUG_MON_SEL		0x0100
-// #define PINCTRL_OFFSET_DIR_0				0x0140
-// #define PINCTRL_OFFSET_DIR_0_SET			0x0144
-// #define PINCTRL_OFFSET_DIR_0_CLR			0x0148
-// #define PINCTRL_OFFSET_DIR_1				0x0150
-// #define PINCTRL_OFFSET_DIR_1_SET			0x0154
-// #define PINCTRL_OFFSET_DIR_1_CLR			0x0158
-// #define PINCTRL_OFFSET_DIR_2				0x0160
-// #define PINCTRL_OFFSET_DIR_2_SET			0x0164
-// #define PINCTRL_OFFSET_DIR_2_CLR			0x0168
-// #define PINCTRL_OFFSET_DIR_3				0x0170
-// #define PINCTRL_OFFSET_DIR_3_SET			0x0174
-// #define PINCTRL_OFFSET_DIR_3_CLR			0x0178
-// #define PINCTRL_OFFSET_DIR_4				0x0180
-// #define PINCTRL_OFFSET_DIR_4_SET			0x0184
-// #define PINCTRL_OFFSET_DIR_4_CLR			0x0188
+#define PINCTRL_BASE_ADDR DT_INST_REG_ADDR(0)
 
 #define PINCTRL_OFFSET_MODE_0  0x0300
 #define PINCTRL_OFFSET_MODE_1  0x0310
@@ -81,92 +37,6 @@
 #define PINCTRL_OFFSET_MODE_20 0x0440
 #define PINCTRL_OFFSET_MODE_21 0x0450
 #define PINCTRL_OFFSET_MODE_22 0x0460
-
-// #define PINCTRL_OFFSET_IES_CFG_0			0x0410
-// #define PINCTRL_OFFSET_IES_CFG_1			0x0420
-// #define PINCTRL_OFFSET_PINCTRL_BANK         0x0430
-// #define PINCTRL_OFFSET_PINCTRL_TM           0x0440
-// #define PINCTRL_OFFSET_MISC_CFG	            0x0450
-// #define PINCTRL_OFFSET_MODE_CFG             0x0460
-// #define PINCTRL_OFFSET_SMT_CFG_0			0x0470
-// #define PINCTRL_OFFSET_SMT_CFG_1			0x0480
-// #define PINCTRL_OFFSET_TDSEL_CFG_0			0x0510
-// #define PINCTRL_OFFSET_TDSEL_CFG_2			0x0520
-// #define PINCTRL_OFFSET_TDSEL_CFG_3			0x0530
-// #define PINCTRL_OFFSET_TDSEL_CFG_4			0x0540
-// #define PINCTRL_OFFSET_TDSEL_CFG_5			0x0550
-// #define PINCTRL_OFFSET_TDSEL_CFG_6			0x0560
-// #define PINCTRL_OFFSET_TDSEL_CFG_1			0x05a0
-// #define PINCTRL_OFFSET_RDSEL_CFG_0			0x0610
-// #define PINCTRL_OFFSET_RDSEL_CFG_1			0x0620
-// #define PINCTRL_OFFSET_RDSEL_CFG_2			0x0630
-// #define PINCTRL_OFFSET_RDSEL_CFG_3			0x0640
-// #define PINCTRL_OFFSET_RDSEL_CFG_4			0x0650
-// #define PINCTRL_OFFSET_RDSEL_CFG_5			0x0660
-// #define PINCTRL_OFFSET_RDSEL_CFG_6			0x0670
-// #define PINCTRL_OFFSET_RDSEL_CFG_7			0x0680
-// #define PINCTRL_OFFSET_RDSEL_CFG_8			0x0690
-// #define PINCTRL_OFFSET_RDSEL_CFG_9			0x06a0
-// #define PINCTRL_OFFSET_DRV_CFG_0			0x0710
-// #define PINCTRL_OFFSET_DRV_CFG_0_SET		0x0714
-// #define PINCTRL_OFFSET_DRV_CFG_0_CLR		0x0718
-// #define PINCTRL_OFFSET_DRV_CFG_1			0x0720
-// #define PINCTRL_OFFSET_DRV_CFG_1_SET		0x0724
-// #define PINCTRL_OFFSET_DRV_CFG_1_CLR		0x0728
-// #define PINCTRL_OFFSET_DRV_CFG_2			0x0730
-// #define PINCTRL_OFFSET_DRV_CFG_2_SET		0x0734
-// #define PINCTRL_OFFSET_DRV_CFG_2_CLR		0x0738
-// #define PINCTRL_OFFSET_DRV_CFG_3			0x0740
-// #define PINCTRL_OFFSET_DRV_CFG_3_SET		0x0744
-// #define PINCTRL_OFFSET_DRV_CFG_3_CLR		0x0748
-// #define PINCTRL_OFFSET_DRV_CFG_4			0x0750
-// #define PINCTRL_OFFSET_DRV_CFG_4_SET		0x0754
-// #define PINCTRL_OFFSET_DRV_CFG_4_CLR		0x0758
-// #define PINCTRL_OFFSET_DRV_CFG_5			0x0760
-// #define PINCTRL_OFFSET_DRV_CFG_5_SET		0x0764
-// #define PINCTRL_OFFSET_DRV_CFG_5_CLR		0x0768
-// #define PINCTRL_OFFSET_DRV_CFG_6			0x0770
-// #define PINCTRL_OFFSET_DRV_CFG_6_SET		0x0774
-// #define PINCTRL_OFFSET_DRV_CFG_6_CLR		0x0778
-// #define PINCTRL_OFFSET_PULL_EN_0			0x0860
-// #define PINCTRL_OFFSET_PULL_EN_0_SET		0x0864
-// #define PINCTRL_OFFSET_PULL_EN_0_CLR		0x0868
-// #define PINCTRL_OFFSET_PULL_EN_1			0x0870
-// #define PINCTRL_OFFSET_PULL_EN_1_SET		0x0874
-// #define PINCTRL_OFFSET_PULL_EN_1_CLR		0x0878
-// #define PINCTRL_OFFSET_PULL_EN_2			0x0880
-// #define PINCTRL_OFFSET_PULL_EN_2_SET		0x0884
-// #define PINCTRL_OFFSET_PULL_EN_2_CLR		0x0888
-// #define PINCTRL_OFFSET_PULL_EN_3			0x0890
-// #define PINCTRL_OFFSET_PULL_EN_3_SET		0x0894
-// #define PINCTRL_OFFSET_PULL_EN_3_CLR		0x0898
-// #define PINCTRL_OFFSET_PULL_EN_4			0x08a0
-// #define PINCTRL_OFFSET_PULL_EN_4_SET		0x08a4
-// #define PINCTRL_OFFSET_PULL_EN_4_CLR		0x08a8
-// #define PINCTRL_OFFSET_PULL_SEL_0			0x0900
-// #define PINCTRL_OFFSET_PULL_SEL_0_SET		0x0904
-// #define PINCTRL_OFFSET_PULL_SEL_0_CLR		0x0908
-// #define PINCTRL_OFFSET_PULL_SEL_1			0x0910
-// #define PINCTRL_OFFSET_PULL_SEL_1_SET		0x0914
-// #define PINCTRL_OFFSET_PULL_SEL_1_CLR		0x0918
-// #define PINCTRL_OFFSET_PULL_SEL_2			0x0920
-// #define PINCTRL_OFFSET_PULL_SEL_2_SET		0x0924
-// #define PINCTRL_OFFSET_PULL_SEL_2_CLR		0x0928
-// #define PINCTRL_OFFSET_PULL_SEL_3			0x0930
-// #define PINCTRL_OFFSET_PULL_SEL_3_SET		0x0934
-// #define PINCTRL_OFFSET_PULL_SEL_3_CLR		0x0938
-// #define PINCTRL_OFFSET_PULL_SEL_4			0x0940
-// #define PINCTRL_OFFSET_PULL_SEL_4_SET		0x0944
-// #define PINCTRL_OFFSET_PULL_SEL_4_CLR		0x0948
-// #define PINCTRL_OFFSET_SEC_CFG_1			0x0950
-// #define PINCTRL_OFFSET_SEC_CFG_0			0x0960
-// #define PINCTRL_OFFSET_MISC_DUMMY			0x09d0
-// #define PINCTRL_OFFSET_BIAS_CFG             0x09f0
-
-typedef struct {
-	uint16_t max_pin;
-	uint16_t max_func;
-} pinctrl_mtk_config_t;
 
 static const pinctrl_mtk_config_t pinctrl_mtk_config = {
 	.max_pin = 177,
@@ -225,8 +95,9 @@ static int pinctrl_set_func(uint16_t pin, uint16_t func)
 	uint32_t val;
 	uint32_t offset;
 
-	if ((pin >= pinctrl_mtk_config.max_pin) || (func >= pinctrl_mtk_config.max_func))
+	if ((pin >= pinctrl_mtk_config.max_pin) || (func >= pinctrl_mtk_config.max_func)) {
 		return -EINVAL;
+	}
 
 	offset = to_mode_offset(pin);
 
@@ -243,14 +114,16 @@ static int pinctrl_set_func(uint16_t pin, uint16_t func)
 
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
 {
-    ARG_UNUSED(reg);
+	while (pin_cnt > 0) {
+		int ret;
 
-	int ret;
+		ret = pinctrl_set_func(pins->pin, pins->func);
+		if (ret != 0) {
+			return ret;
+		}
 
-    for (int idx = 0; idx < pin_cnt; idx++) {
-		ret = pinctrl_set_func(pins[idx].pin, pins[idx].func);
-		if (ret != 0)
-			return (ret);
+		pins++;
+		pin_cnt--;
 	}
 
 	return 0;
